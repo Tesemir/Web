49469670f7ae2332143bf6a5940df64ed9771288
Tesemir <madi41674@gmail.com>
2024-11-12 00:00:34 +0500

    mobile navbar added

--- sound.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('play-sound-btn').addEventListener('click', function() {
-    var sound = document.getElementById('notificationSound');
-    sound.currentTime = 0;
-    sound.play();
-});
